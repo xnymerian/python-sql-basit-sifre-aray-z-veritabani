1926237324f639e0dfe2a4e4085a286161bf158d
Mehmet Güven <92626875+xnymerian@users.noreply.github.com>
2022-07-11 13:31:26 +0300

    Create .sql

---
+++ .sql
@@ -0,0 +1,33 @@
+import sqlite3
+
+db = sqlite3.connect("vt.sqlite")
+
+im = db.cursor()
+
+im.execute("""CREATE TABLE IF NOT EXISTS users
+    (user_name, password)""")
+
+datas = [
+            ("ahmet123", "12345678"),
+            ("mehmet321", "87654321"),
+            ("selin456", "123123123")
+          ]
+
+for i in datas:
+    im.execute("""INSERT INTO kullanicilar VALUES (?, ?)""", i)
+
+db.commit()
+while 1==1:
+      kull = input("User name: ")
+      paro = input("Password: ")
+
+      im.execute("""SELECT * FROM kullanicilar WHERE
+      user_name = ? AND password = ?""", (kull, paro))
+
+      data = im.fetchone()
+
+      if data:
+         print("Welcome the programme {}!".format(data[0]))
+         break
+      else:
+         print("Parola veya kullanıcı adı yanlış!")
